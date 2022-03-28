df0e4d0b886d736a5a6f184d5a6c06e28602877b
Joelo Sumerente <49916469+Joelo21@users.noreply.github.com>
2022-03-28 12:00:14 -0500

    Actualizando Dashboard-19

--- scripts/read_data.R
+++ scripts/read_data.R
@@ -2,7 +2,7 @@
 
 # Data departamental
 read_data_dpto <- function() {
-  data_dpto <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source2_siscovid/output/data_regional.csv", keepLeadingZeros = TRUE)
+  data_dpto <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source2_siscovid/output/data_regional.csv", keepLeadingZeros = TRUE)
   data_dpto$fecha <- as.Date(data_dpto$fecha)
   data_dpto <- subset(data_dpto, fecha > as.Date("2020-03-12") & fecha < Sys.Date() - 1)
   data_dpto <- mutate(data_dpto, xposi=log10(total_positivo), xini = log10(total_inicio))
@@ -14,7 +14,7 @@ read_data_dpto <- function() {
 
 # Data provincial
 read_data_prov <- function() {
-  data_prov <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source2_siscovid/output/data_provincial.csv", keepLeadingZeros = TRUE)
+  data_prov <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source2_siscovid/output/data_provincial.csv", keepLeadingZeros = TRUE)
   data_prov$fecha <- as.Date(data_prov$fecha)
   data_prov <- subset(data_prov, fecha > as.Date("2020-03-12") & fecha < Sys.Date() - 1)
   data_prov <- mutate(data_prov, posi_molecular_percent = posi_molecular*100)
@@ -24,7 +24,7 @@ read_data_prov <- function() {
 
 # Data distrital
 read_data_dis <- function() {
-  data_dis <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source2_siscovid/output/data_distrital.csv", keepLeadingZeros = TRUE)
+  data_dis <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source2_siscovid/output/data_distrital.csv", keepLeadingZeros = TRUE)
   data_dis$fecha <- as.Date(data_dis$fecha)
   data_dis <- subset(data_dis, fecha > as.Date("2020-03-12") & fecha < Sys.Date() -1)
   data_dis <- mutate(data_dis, IDDIST = ubigeo)
@@ -38,13 +38,13 @@ read_data_dis <- function() {
 # Data de mapa distrital
 
 read_data_map_district <- function() {
-  cusco_map_district <- jsonlite::fromJSON("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source4_mapas/districts.geojson", simplifyVector = FALSE)
+  cusco_map_district <- jsonlite::fromJSON("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source4_mapas/districts.geojson", simplifyVector = FALSE)
 }
 
 
 # Data provincial
 read_data_prov <- function() {
-  data_prov <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source2_siscovid/output/data_provincial.csv", keepLeadingZeros = TRUE)
+  data_prov <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source2_siscovid/output/data_provincial.csv", keepLeadingZeros = TRUE)
   data_prov$fecha <- as.Date(data_prov$fecha)
   data_prov <- subset(data_prov, fecha > as.Date("2020-03-12") & fecha < Sys.Date() - 1)
   data_prov <- mutate(data_prov, posi_molecular_percent = posi_molecular*100)
@@ -55,7 +55,7 @@ read_data_prov <- function() {
 # Data camas
 read_data_beds <- function() {
   
-  data_beds <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source1_camas/output/camas.csv")
+  data_beds <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source1_camas/output/camas.csv")
   data_beds$fecha <- as.Date(data_beds$fecha)
   data_beds <- mutate(data_beds, UCI_percent = UCI*100)  
   data_beds <- mutate(data_beds, NOUCI_percent = NOUCI*100)  
@@ -67,20 +67,20 @@ read_data_beds <- function() {
 
 # Data valores semáforo provincial,
 read_semaforo <- function() {
-  data_semaforo <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source3_semaforo/traffic_light.csv")
+  data_semaforo <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source3_semaforo/traffic_light.csv")
 }
 
 
 # Data valores semáforo distrital,
 read_semaforo_dis <- function() {
-  data_semaforo_dis <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source3_semaforo/traffic_light_distrital.csv")
+  data_semaforo_dis <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source3_semaforo/traffic_light_distrital.csv")
 }
 
 
 # Data vacunaci�n
 
 read_data_vacunas <- function() {
-  data_vacunas <- fread("https://raw.githubusercontent.com/gorecuscogrds/dashboard-covid-geresa/main/data/source5_vacunas/vacunacion.csv")
+  data_vacunas <- fread("https://raw.githubusercontent.com/Joelo21/GERESA_dashboard/main/data/source5_vacunas/vacunacion.csv")
   
   str(data_vacunas$fecha)
   data <- xts(x = data_vacunas$vacunados, order.by = data_vacunas$fecha)
